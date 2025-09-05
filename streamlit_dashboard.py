7e63df53542941faff27c588120eca8a3541ba8b
Hinal Bhagat <hinal.t8633@ltimindtree.com>
2025-09-05 16:00:34 +0530

    initial commit

---
+++ streamlit_dashboard.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Set the title
+st.title("👋 Hello World!")
+
+# Display big text
+st.markdown("<h1 style='text-align: center; color: blue;'>HELLO WORLD</h1>", unsafe_allow_html=True)
